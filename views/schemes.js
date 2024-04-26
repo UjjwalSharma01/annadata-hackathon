8de87d8169451d3fe849b41ef1d259695ee34af7
Geetansh <145845798+Geetansh01@users.noreply.github.com>
2024-04-27 01:29:37 +0530

    added yt links in schemes

--- views/schemes.js
+++ views/schemes.js
@@ -1,15 +1,15 @@
-const allSchemes = [
-    { name: 'Pradhan Mantri Kisan Samman Nidhi (PM-KISAN)', link: 'https://pmkisan.gov.in/' },
-    { name: 'Pradhan Mantri Fasal Bima Yojana (PMFBY)', link: 'https://pmfby.gov.in/' },
-    { name: 'National Mission for Sustainable Agriculture (NMSA)', link: 'https://nmsa.dac.gov.in/' },
-    { name: 'Paramparagat Krishi Vikas Yojana (PKVY)', link: 'https://www.pmkvyofficial.org/' },
-    { name: 'Soil Health Card Scheme', link: 'https://soilhealth.dac.gov.in/' },
-    { name: 'Rashtriya Krishi Vikas Yojana (RKVY)', link: 'https://rkvy.nic.in/' },
-    { name: 'National Livestock Mission (NLM)', link: 'https://nlm.udyamimitra.in/' },
-    { name: 'National Beekeeping and Honey Mission (NBHM)', link: 'https://nbb.gov.in/default.html' },
-    { name: 'Animal Husbandry Infrastructure Development Fund (AHIDF)', link: 'https://ahidf.udyamimitra.in/' },
-    { name: 'Kisan Credit Card (KCC)', link: 'https://upagripardarshi.gov.in/staticpages/KisanCreditCard.aspx' },
-];
+// const allSchemes = [
+//     { name: 'Pradhan Mantri Kisan Samman Nidhi (PM-KISAN)', link: 'https://pmkisan.gov.in/' },
+//     { name: 'Pradhan Mantri Fasal Bima Yojana (PMFBY)', link: 'https://pmfby.gov.in/' },
+//     { name: 'National Mission for Sustainable Agriculture (NMSA)', link: 'https://nmsa.dac.gov.in/' },
+//     { name: 'Paramparagat Krishi Vikas Yojana (PKVY)', link: 'https://www.pmkvyofficial.org/' },
+//     { name: 'Soil Health Card Scheme', link: 'https://soilhealth.dac.gov.in/' },
+//     { name: 'Rashtriya Krishi Vikas Yojana (RKVY)', link: 'https://rkvy.nic.in/' },
+//     { name: 'National Livestock Mission (NLM)', link: 'https://nlm.udyamimitra.in/' },
+//     { name: 'National Beekeeping and Honey Mission (NBHM)', link: 'https://nbb.gov.in/default.html' },
+//     { name: 'Animal Husbandry Infrastructure Development Fund (AHIDF)', link: 'https://ahidf.udyamimitra.in/' },
+//     { name: 'Kisan Credit Card (KCC)', link: 'https://upagripardarshi.gov.in/staticpages/KisanCreditCard.aspx' },
+// ];
 
 const allNgos = [
     { name: 'Aahwahan', link: 'https://www.aahwahan.com/' },
@@ -23,6 +23,20 @@ const allNgos = [
     { name: 'Nav Bharat Jagriti Kendra (NBJK)', link: 'https://www.nbjk.org/' },
     // Add more NGOs as needed
 ];
+
+const allSchemes = [
+    { name: 'Pradhan Mantri Kisan Samman Nidhi (PM-KISAN)', link: 'https://www.youtube.com/watch?v=beOsC0dhncc' },
+    { name: 'Pradhan Mantri Fasal Bima Yojana (PMFBY)', link: 'https://www.youtube.com/watch?v=kAlNM6G6cC4' },
+    { name: 'National Mission for Sustainable Agriculture (NMSA)', link: 'https://www.youtube.com/watch?v=SFKz_aEFfZw' },
+    { name: 'Paramparagat Krishi Vikas Yojana (PKVY)', link: 'https://www.youtube.com/watch?v=_JTaN927MKc' },
+    { name: 'Soil Health Card Scheme', link: 'https://www.youtube.com/watch?v=ViYTVlOA1FY' },
+    { name: 'Rashtriya Krishi Vikas Yojana (RKVY)', link: 'https://www.youtube.com/watch?v=beOsC0dhncc' },
+    { name: 'National Livestock Mission (NLM)', link: 'https://youtu.be/jGMnxpLjQ4s?si=uSqIPJ_xkuu0ezfp' },
+    { name: 'National Beekeeping and Honey Mission (NBHM)', link: 'https://www.youtube.com/watch?v=beOsC0dhncc' },
+    { name: 'Animal Husbandry Infrastructure Development Fund (AHIDF)', link: 'https://www.youtube.com/watch?v=beOsC0dhncc' },
+    { name: 'Kisan Credit Card (KCC)', link: 'https://www.youtube.com/watch?v=beOsC0dhncc' },
+];
+
 
 window.onload = function() {
     const schemesElement = document.querySelector('.schemes');
